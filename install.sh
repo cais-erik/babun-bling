30b3edd010a1fe3ad962d8b0bc92405a7076357b
U-CAISGROUP\elumpkin <elumpkin@C5-202-ELUM.CAISgroup.local>
2015-09-24 14:55:54 -0500

    tweeaking install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 cd $ZSH_CUSTOM/plugins/
 git clone https://github.com/cais-erik/babun-bling.git
-cp $ZSH_CUSTOM/plugins/babun-bling/.zshrc.orig ~/
+mv ~/.zshrc ~/.zshrc.replaced
+cp $ZSH_CUSTOM/plugins/babun-bling/.zshrc.orig ~/.zshrc
