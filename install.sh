7cbcc7b8dda073fff5b0a228e58402f3d2434648
U-CAISGROUP\elumpkin <elumpkin@C5-202-ELUM.CAISgroup.local>
2015-09-24 15:00:14 -0500

    adding behavior to stash a copy of comp dump upon install

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ cd $ZSH_CUSTOM/plugins/
 git clone https://github.com/cais-erik/babun-bling.git
 mv ~/.zshrc ~/.zshrc.replaced
 cp $ZSH_CUSTOM/plugins/babun-bling/.zshrc.orig ~/.zshrc
+cp $ZSH_COMPDUMP ${ZSH_COMPDUMP}.save
+chmod ugo-wx ${ZSH_COMPDUMP}.save
