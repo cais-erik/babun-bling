612ed91544d7f6f92ed1c41ec9e4a841036d0b93
Erik Lumpkin <elumpkin@caisgroup.com>
2015-10-29 17:35:00 -0500

    qa log alias

--- babun-bling.plugin.zsh
+++ babun-bling.plugin.zsh
@@ -3,3 +3,4 @@
 alias bbCompRegen="autoload -U compinit; compinit -i -d ${ZSH_COMPDUMP}"
 alias bbRevertZshrc="cp ${ZSH_CUSTOM}/plugins/babun-bling/.zshrc.orig ~/.zshrc"
 alias bbUpdate="zsh -c 'cd ${ZSH_CUSTOM}/plugins/babun-bling; git pull'"
+alias qaLog=" curl -k --basic -u reader:CAIS6xBQ https://50.75.201.253:9090/tc-logs/caisDebug.log"
